4345e9270313ef6ea39c3cc53521d7d1df9daebf
AYUSH PANDEY <ayushp@umbrellainfocare.com>
2025-07-19 22:27:30 +0530

    code changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,32 @@ module "workload" {
 
   account_customizations_name = "workload"
 }
+
+
+module "workloadaccount" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail             = "rk45joshi@gmail.com"
+    AccountName              = "workload-aft-account"
+    ManagedOrganizationalUnit = "workload"
+    SSOUserEmail             = "rk45joshi@gmail.com"
+    SSOUserFirstName         = "workload-account"
+    SSOUserLastName          = "AFT-account"
+  }
+
+  account_tags = {
+    "AFT-Accounts" = "workload"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "AFT"
+    change_reason       = "AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "workloadaccount"
+}
